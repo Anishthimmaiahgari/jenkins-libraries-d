2ba1d2b1e4a931c08798005271123b14863b5055
Anishthimmaiahgari <anish.thimmaiahgari@bilvantis.io>
2024-11-27 10:27:44 +0530

    Update hiMan.groovy

--- vars/hiMan.groovy
+++ vars/hiMan.groovy
@@ -1,3 +1,7 @@
 def call() {
-  sh 'echo jen'
+  sh ''' gcloud auth activate-service-account --key-file=${SERVICE_ACCOUNT_JSON}
+                        gcloud config set project ${PROJECT_ID}
+                        gcloud auth configure-docker --quiet
+                        docker build -t ${IMAGE_NAME_QA} .
+                        docker push ${IMAGE_NAME_QA}'''
 }
